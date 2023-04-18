54a11af51a4da47d9882d69f3bd17fd36bf7fb33
Vitor Pedra <vitor.s.pedra@gmail.com>
2023-04-18 13:25:16 -0300

    full project

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:16.8-alpine
+
+WORKDIR /usr/app
+
+COPY package.json package-lock.json ./
+
+RUN npm install
+
+COPY . .
+
+
+EXPOSE 3333
+
+
+CMD [ "npm","start" ]
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require("cors")
+const PORT = process.env.PORT || 3333
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.get("/docker",(req,res)=>{
+    res.status(200).send("endpoint docker rodando bunitão!!")
+})
+app.get("/",(req,res)=>{
+    res.status(200).send("rodando na maciota!!")
+})
+app.listen(PORT,()=>{
+    console.log("rodando na 3000")
+})
